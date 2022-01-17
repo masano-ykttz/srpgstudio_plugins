9e33ce1024a5e7e6b671d6ca694797cb071e344d
masano-ykttz <47180128+masano-ykttz@users.noreply.github.com>
2022-01-17 20:42:02 +0900

    プラグイン導入後にユーザーが調整しやすいようコメントアウトを中心に修正

--- script/custom_hpbar_number.js
+++ script/custom_hpbar_number.js
@@ -1,19 +1,27 @@
-//マップ上でのHP表示設定で「数値」を選択した場合に表示されるHPバーの改変
+//マップ上でのHP表示設定で「数値」を選択した場合に表示されるHPバーを細くするプラグイン
 //公式のsingleton-currentmap.js参照
+//一部変数を変更することで表示方法を調整できます。
+//利用に関する規約は下記URLのリポジトリ内にあるREADMEをご確認ください。
+//https://github.com/masano-ykttz/srpgstudio_plugins
 
 MapHpDecorator._setupDecorationFromType = function(type) {
 	var obj = root.getHpDecoration(type);
         var pos = this._getPos();  //表示位置取得
-  
-        var width = 32; //横幅指定
-	var height = 4;  //高さ指定（本来の高さは10）
+	
+	/*****************************  調整可能箇所ここから  ******************************/
+	var width = 32; //HPゲージの横幅指定
+	var height = 4;  //HPゲージの高さ指定（本来の高さは10）
+	/*****************************  調整可能箇所ここまで  *****************************/
 	
 	var color = this._getColor(type);  //色取得
 	var alpha = this._getAlpha(type);  //透明度取得
-	var strokeColor = 0xff;
-	var strokeAlpha = 255;
-	var hpType = EnvironmentControl.getMapUnitHpType();
 	
+	/*****************************  調整可能箇所ここから  ******************************/
+	var strokeColor = 0xff;            //ゲージの外枠の色指定（初期値0xff）
+	var strokeAlpha = 255;             //ゲージの外枠の透明度指定。値が小さいほど透明になります。（初期値255）
+	/*****************************  調整可能箇所ここまで  *****************************/
+	
+	var hpType = EnvironmentControl.getMapUnitHpType();
 	obj.beginDecoration();
 	
 	if (hpType === 0) {
@@ -22,9 +30,11 @@ MapHpDecorator._setupDecorationFromType = function(type) {
 		obj.setStrokeInfo(strokeColor, strokeAlpha, 1, true);
 		obj.addRectangle(pos.x, pos.y, width, height); //四角の描画
 		
-		//数値表示だけずらすとき用パラメータ
-		var pos2x = 0; //数値のx座標をずらす
-		var pos2y = 4; //数値のy座標をずらす
+		/*****************************  調整可能箇所ここから  ******************************/
+		//数値の表示をずらすとき用パラメータ
+		var pos2x = 0; //数値のx座標をずらす。数値が大きいほど右にずれる
+		var pos2y = 4; //数値のy座標をずらす。数値が大きいほど下にずれる
+		/*****************************  調整可能箇所ここまで  *****************************/
 		
 		obj.addHp(pos.x - pos2x, pos.y - pos2y, this._getNumberColorIndex(hpType));  //数字(HP数値)の描画
 	}
@@ -36,9 +46,15 @@ MapHpDecorator._setupDecorationFromType = function(type) {
 };
 
 MapHpDecorator._getColor = function(type) {
+  //残りHPの割合によるゲージの色を指定します。
+  //[HP全回復時の色,HP少し減少時の色,HPそこそこ減少時の色,残りHPが少ない時の色]の順に指定できます。
+  //0xの後の6桁を0~Fで指定。0x[R][G][B]となる
+  //RGBについては「RGB 数値」とかで検索すればどの数値でどの色になるか分かります。
   //HPの割合による色指定(0xの後の6桁を0~Fで指定。0x[R][G][B]となる)
   var arr = [0x00ffff, 0x00ff00, 0xffff00, 0xff0000];
 
+　//var arr = [0x00ffff, 0x00ff00, 0xffff00, 0xff0000];  //初期値（バックアップ用）
+	
   return arr[type];
 };
 	
@@ -48,13 +64,14 @@ MapHpDecorator._getAlpha = function(type) {
 };
 
 MapHpDecorator._getNumberColorIndex = function(type) {
+  //表示される数字。「リソース使用箇所->UI->数字」で指定された数字画像のIDを指定
   return 0;
 };
 
 //表示位置の指定。
 MapHpDecorator._getPos = function() {
-  var x = 1;   //x座標、大きいほど右に寄る
-  var y = 28;  //y座標、大きいほど下に寄る（本来の高さは20）
+  var x = 1;   //x座標、大きいほど右に移動
+  var y = 28;  //y座標、大きいほど下に移動（SRPGStudio本来の初期値は20）
 
   //mapchipの1マスが32x32(px)でないときの調整用
   if (GraphicsFormat.MAPCHIP_WIDTH !== 32 || GraphicsFormat.MAPCHIP_HEIGHT !== 32) {
